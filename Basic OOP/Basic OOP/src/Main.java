b05d86067f98ac31bc8d8114a351e367841d49d1
Lutfan Alaudin Naja <lutfanalaudin@gmail.com>
2025-11-10 22:46:33 +0700

    Add initial Mahasiswa class and Main class for student object instantiation and display

---
+++ Basic OOP/Basic OOP/src/Main.java
@@ -0,0 +1,28 @@
+// Membuat class sebagai template
+class Mahasiswa {
+  String nama;
+  String NIM;
+  String jurusan;
+  double IPK;
+}
+
+public class Main {
+  public static void main(String[] args) {
+
+    // Instansiasi / membuat object
+    Mahasiswa mahasiswa1 = new Mahasiswa();
+    mahasiswa1.nama = "Lutfan";
+    mahasiswa1.IPK = 4.00;
+
+    System.out.println(mahasiswa1.nama);
+    System.out.println(mahasiswa1.NIM = "2400018032");
+    System.out.println(mahasiswa1.jurusan);
+    System.out.println(mahasiswa1.IPK);
+
+    Mahasiswa mahasiswa2 = new Mahasiswa();
+    mahasiswa2.nama = "Naja";
+
+    System.out.println(mahasiswa2.nama);
+    System.out.println(mahasiswa2.NIM = "2400018033");
+  }
+}
