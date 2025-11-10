f38c43a78f5dbfa42db74f483c895c55d5a0ab6e
Lutfan Alaudin Naja <lutfanalaudin@gmail.com>
2025-11-10 23:29:53 +0700

    Update Mahasiswa constructor to accept NIM and adjust Main class instantiation

--- Constructor/src/Main.java
+++ Constructor/src/Main.java
@@ -1,19 +1,21 @@
 // Class  Mahasiswa
 class Mahasiswa{
   String nama;
+  String nim;
 
   // Constructor
-  Mahasiswa(String data_nama) {
+  Mahasiswa(String data_nama, String data_nim) {
     nama = data_nama;
+    nim = data_nim;
+
+    System.out.println("Nama : " + nama);
+    System.out.println("NIM : " + nim);
   }
 }
 
 public class Main {
   public static void main(String[] args) {
-    Mahasiswa mahasiswa1 = new Mahasiswa("Lutfan");
-    Mahasiswa mahasiswa2 = new Mahasiswa("Naja");
-
-    System.out.println(mahasiswa1.nama);
-    System.out.println(mahasiswa2.nama);
+    Mahasiswa mahasiswa1 = new Mahasiswa("Lutfan", "2400018032");
+    Mahasiswa mahasiswa2 = new Mahasiswa("Naja", "2400018033");
   }
 }
