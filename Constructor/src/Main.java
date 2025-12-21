c3b5a5421f28cdd571b7a4a2c35ec97030ce9876
Lutfan Alaudin Naja <lutfanalaudin@gmail.com>
2025-12-22 00:22:01 +0700

    Refactor Mahasiswa constructor visibility and clean up instantiation in Main class

--- Constructor/src/Main.java
+++ Constructor/src/Main.java
@@ -1,10 +1,10 @@
-// Class  Mahasiswa
+// Class Mahasiswa
 class Mahasiswa{
   String nama;
   String nim;
 
   // Constructor
-  Mahasiswa(String data_nama, String data_nim) {
+  public Mahasiswa(String data_nama, String data_nim) {
     nama = data_nama;
     nim = data_nim;
 
@@ -15,7 +15,7 @@ class Mahasiswa{
 
 public class Main {
   public static void main(String[] args) {
-    Mahasiswa mahasiswa1 = new Mahasiswa("Lutfan", "2400018032");
-    Mahasiswa mahasiswa2 = new Mahasiswa("Naja", "2400018033");
+    new Mahasiswa("Lutfan", "2400018032");
+    new Mahasiswa("Naja", "2400018033");
   }
 }
